728090df8a5affd04d4e30403f24a9c3818c5e8e
Gvrzizo <s18110629@outlook.com>
2026-05-20 12:11:45 +0800

    Test personal api access

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
+import requests
+import json
+
+API_BASE_URL = "https://maimai.lxns.net/api/v0/chunithm"
+API_KEY = ""
+
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
@@ -19,6 +25,30 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    @filter.command("bind")
+    async def bind(self, event: AstrMessageEvent, usrapi: str):
+        API_KEY = usrapi
+        url = f"{API_BASE_URL}/player"
+        queryHeaders = {
+            "X-User-Token": API_KEY
+        }
+        try:
+            response = requests.get(url, headers=queryHeaders)
+            response.raise_for_status() # 检查HTTP请求是否出错
+            data = response.json()
+
+            if data.get("success"):
+                # 请求成功，处理data字段中的数据
+                yield event.plain_result(f"API绑定为用户{data.data.name}") # 发送一条纯文本消息
+            else:
+                yield event.plain_result(f"API返回错误: {data.get('message')}")
+        except requests.exceptions.RequestException as e:
+            yield event.plain_result(f"网络请求出错: {e}")
+
+    @filter.command("caj30")
+    async def caj30(self, event: AstrMessageEvent):
+        pass
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
