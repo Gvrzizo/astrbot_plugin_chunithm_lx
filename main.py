fb6deaa3e9a09f79a06c4fa954f60ae9af257084
Gvrzizo <s18110629@outlook.com>
2026-05-20 22:14:42 +0800

    Fix

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ class Lauretta(Star):
             return high
 
     def render_aj30_image(self, player_name: str, top30: list, aj30_avg: float, out_path: str, sender_id: str):
-        base_dir = os.path.dirname(self.storagePath)
+        base_dir = self.storagePath
         env = Environment(loader=FileSystemLoader(base_dir), autoescape=True)
         template = env.get_template("AJ30.html")
 
