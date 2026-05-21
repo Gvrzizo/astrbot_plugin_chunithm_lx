d81478d48d5fa18d1a1337c12d35f33cd6ccbb1a
Gvrzizo <s18110629@outlook.com>
2026-05-21 09:28:17 +0800

    Tweak

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ class Lauretta(Star):
             records = top30,
             aj30_avg = aj30_avg,
         )
-        hti = Html2Image(output_path = out_path, size = (1800, 1000), custom_flags=['--force-device-scale-factor=4'])
+        hti = Html2Image(output_path = out_path, size = (1800, 1200), custom_flags=['--force-device-scale-factor=4'])
         hti.screenshot(
             html_str=html,
             save_as=f"{sender_id}_AJ30.png",
