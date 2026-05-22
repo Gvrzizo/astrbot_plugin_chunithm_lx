50552680f589769a61fc985bfbcdbaacb86f10f3
Gvrzizo <s18110629@outlook.com>
2026-05-22 14:06:03 +0800

    Tweak

--- main.py
+++ main.py
@@ -390,7 +390,7 @@ class Lauretta(Star):
         for b in cc_blocks:
             songnum = len(b["songs"])
             rows += (songnum + songs_per_row - 1) // songs_per_row
-        height = 150 + rows * 180 + len(cc_blocks) * 20
+        height = 180 + rows * 185 + len(cc_blocks) * 30
 
         hti = Html2Image(
             output_path=out_path,
