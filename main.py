230f3512913fd6416fb7225bdd42c06d9c09d8cd
Gvrzizo <s18110629@outlook.com>
2026-09-17 09:37:00 +0800

    chore: bump version to 0.4.0

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def admin_command(name: str, **kwargs):
     return decorator
 
 
-@register("chunithm_lx", "Lauretta", "中二节奏机器人", "0.3.2")
+@register("chunithm_lx", "Lauretta", "中二节奏机器人", "0.4.0")
 class Lauretta(Star):
     TOKEN_REFRESH_INTERVAL = 7 * 24 * 3600
 
